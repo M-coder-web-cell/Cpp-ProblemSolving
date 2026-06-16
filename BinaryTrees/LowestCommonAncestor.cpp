32eb097d30e4eeec8ab09eded0280118ad82b9cd
HackRogue <mazumdarh549@gmail.com>
2026-06-17 00:10:56 +0530

    LCA problem

--- BinaryTrees/LowestCommonAncestor.cpp
+++ BinaryTrees/LowestCommonAncestor.cpp
@@ -16,5 +16,18 @@ public:
 };
 
 TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    
+    if(root == NULL) return NULL;
+    if(root == p) return p;
+    if(root == q) return q;
+
+    TreeNode* l = lowestCommonAncestor(root->left, p, q);
+    TreeNode* r = lowestCommonAncestor(root->right, p, q);
+    if(!l && !r) return NULL;
+    if(l && !r) return l;
+    if(!l && r) return r;
+    if(l && r && (l != r)) {
+        return root;
+    } 
+
+    return root;
 }
