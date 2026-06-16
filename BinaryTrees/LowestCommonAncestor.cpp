d1f1a26dbca7298302cb4041ece6b6a42ab3b958
HackRogue <mazumdarh549@gmail.com>
2026-06-16 23:22:52 +0530

    Binary tree and Linkedlist problem solved

---
+++ BinaryTrees/LowestCommonAncestor.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class TreeNode{
+public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    
+}
